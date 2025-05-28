ed10025bf6bfd137381cf187a2dd83fb61776940
newdongjun <shindj4613@outlook.com>
2025-05-28 18:55:40 +0900

    FEAT: audioPlays.ts contents.ts output.mp3

--- src/content/content.ts
+++ src/content/content.ts
@@ -1,3 +1,5 @@
+import playAudioBlob from './audioPlay';
+
 // content.ts
 console.log('손길도우미 확장프로그램이 로드되었습니다.');
 
@@ -177,6 +179,10 @@ function createPopup() {
     setTimeout(() => {
       chrome.runtime.sendMessage({ action: 'captureVisibleTab' }, (dataUrl) => {
         // 캡처 후 팝업 복원
+        {//오디오 플레이 부분 브라우전 정책상 테스트를 위해서 클릭 버튼 눌린 후에 코드를 작성함
+          playAudioBlob();
+          console.log("오디오 재생 함수가 호출되었습니다.");
+        }
         popupContainer.style.opacity = '1';
         if (dataUrl) {
           const img = document.createElement('img');
